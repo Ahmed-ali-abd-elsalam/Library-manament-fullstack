aabe3b3fa3a69214256e3cedb5c5002f9800b4dc
Ahmed Abdesalam <thei3utcher2001@gmail.com>
2025-09-12 20:39:17 +0300

    add Mappers from and to the request and response forms

---
+++ Application/Mappers/BookMapper.cs
@@ -0,0 +1,34 @@
+﻿using Application.DTOs;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Mappers
+{
+    internal static class BookMapper
+    {
+        public static Book DtoToBookMapper(this BookDto dto)
+        {
+            return new Book
+            {
+                Author = dto.Author,
+                Title = dto.Title,
+                PublishedYear = dto.PublishedYear,
+                IsAvailable = true
+            };
+        }
+        public static BookResponseDto BookToDtoMapper(this Book book)
+        {
+            return new BookResponseDto
+            {
+                Author = book.Author,
+                Title = book.Title,
+                PublishedYear = book.PublishedYear,
+                IsAvailable = book.IsAvailable
+            };
+        }
+    }
+}
---
+++ Application/Mappers/MemberMapper.cs
@@ -0,0 +1,32 @@
+﻿using Application.DTOs;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Mappers
+{
+    internal static class MemberMapper
+    {
+        public static Member RegisterDtoToMember(this RegisterMemberDto memberDto)
+        {
+            return new Member {
+                Email = memberDto.Email ,
+                HashedPassword = memberDto.Password,
+                Name =memberDto.Name,
+                Role = memberDto.Role,
+                JoinDate=DateOnly.FromDateTime(DateTime.UtcNow)};
+        }
+        public static MemberResponseDto ToMemberResponseDto(this Member member)
+        {
+            return new MemberResponseDto
+            {
+                Email = member.Email,
+                Role = member.Role,
+                Name = member.Name,
+            };
+        }
+    }
+}
