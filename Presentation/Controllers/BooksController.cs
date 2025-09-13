cc08f9be24566ea0e0bd3e49e24e4761473c1da0
Ahmed Abdesalam <thei3utcher2001@gmail.com>
2025-09-13 10:59:07 +0300

    implemented the books controller

---
+++ Presentation/Controllers/BooksController.cs
@@ -0,0 +1,63 @@
+﻿using Application.DTOs;
+using Application.Services;
+using Domain.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class BooksController :Controller
+    {
+        private readonly IBookService _bookService;
+
+        public BooksController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(statusCode: 200,type: typeof(BooksPaginatedDto))]
+        public async Task<IActionResult> getBooks(int offest=0,int count=100)
+        {
+            return Ok(await _bookService.GetAllBooks(offest, count));
+        }
+        [HttpPost]
+        [ProducesResponseType(statusCode: 200,type: typeof(BookResponseDto))]
+        public async Task<IActionResult> AddBook(BookDto bookDto)
+        {
+            return Ok(await _bookService.AddNewBook(bookDto));
+        }
+        [HttpPut("api/Books/{BookId}")]
+        [ProducesResponseType(statusCode: 200, type: typeof(BookResponseDto))]
+        public async Task<IActionResult> UpdateBook(int BookId,BookDto bookDto)
+        {
+            var book =await _bookService.UpdateBook(bookDto,BookId);
+            if (book == null) return NotFound();
+            return Ok(book);
+        }
+        [HttpDelete("api/Books/{BookId}")]
+        [ProducesResponseType(statusCode: 200, type: typeof(BookResponseDto))]
+        public async Task<IActionResult> Delete(int BookId)
+        {
+            try
+            {
+                var book = await _bookService.DeleteBook(BookId);
+                return Accepted();
+            }
+            catch (BookDoesnotExist e)
+            {
+                return NotFound(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return NoContent();
+            }
+        }
+
+
+
+    }
+}
--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,9 +1,18 @@
+using Application.IRepository;
+using Application.Services;
+using Infrastructure.Data;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<LibraryDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddScoped<IBookRepository,BookRepository>();
+builder.Services.AddScoped<IBookService,BookService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
